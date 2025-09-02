1a07e16bae2014394f3bc84b4cd8e4c567ed239d
press-vm <mitin.vladimir@hotmail.com>
2025-09-02 23:44:36 +0300

    backup.sh add

---
+++ backup.sh
@@ -0,0 +1,3 @@
+tar -czf "backup/remnawave-tg-bot_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz" "remnawave-tg-bot"
+tar -czf "backup/remnawave_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz" "remnawave"
+tar -czf "backup/remnanode_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz" "remnanode"
