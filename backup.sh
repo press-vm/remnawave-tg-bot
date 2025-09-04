2187a7e27b5a3469b9712662b7098a126b6f4508
press-vm <mitin.vladimir@hotmail.com>
2025-09-05 00:37:35 +0300

    Clear old files

--- backup.sh
+++
@@ -1,3 +0,0 @@
-tar -czf "backup/remnawave-tg-bot_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz" "remnawave-tg-bot"
-tar -czf "backup/remnawave_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz" "remnawave"
-tar -czf "backup/remnanode_$(date +"%Y-%m-%d_%H-%M-%S").tar.gz" "remnanode"
